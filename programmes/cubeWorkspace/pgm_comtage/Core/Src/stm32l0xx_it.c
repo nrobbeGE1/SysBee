7fa405af373a1cf5111fc2fd4e58898737ea7b9e
Antoine <antoine.serry@gmail.com>
2021-10-16 16:57:39 +0200

    Signaux visuels et déclenchement d'une alerte à distance

--- programmes/cubeWorkspace/pgm_comtage/Core/Src/stm32l0xx_it.c
+++ programmes/cubeWorkspace/pgm_comtage/Core/Src/stm32l0xx_it.c
@@ -56,6 +56,7 @@
 /* USER CODE END 0 */
 
 /* External variables --------------------------------------------------------*/
+extern TIM_HandleTypeDef htim2;
 extern TIM_HandleTypeDef htim3;
 extern UART_HandleTypeDef huart5;
 /* USER CODE BEGIN EV */
@@ -154,6 +155,20 @@ void USART4_5_IRQHandler(void)
   /* USER CODE BEGIN USART4_5_IRQn 1 */
 
   /* USER CODE END USART4_5_IRQn 1 */
+}
+
+/**
+  * @brief This function handles TIM2 global interrupt.
+  */
+void TIM2_IRQHandler(void)
+{
+  /* USER CODE BEGIN TIM2_IRQn 0 */
+
+  /* USER CODE END TIM2_IRQn 0 */
+  HAL_TIM_IRQHandler(&htim2);
+  /* USER CODE BEGIN TIM2_IRQn 1 */
+
+  /* USER CODE END TIM2_IRQn 1 */
 }
 
 /**
